beda7870d5821d24aa5eb0ef3fe4341c16f663ca
Subodh0 <70047623+Subodh0@users.noreply.github.com>
2023-10-08 13:59:18 +0530

    Hedare component completed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Anita Sah</h1>
+        <h5 className="text-light">Business Analyst</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="MyPic" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+        
+      </div>
+    </header>
   )
 }
 

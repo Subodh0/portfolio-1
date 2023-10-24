1d733209bbddd32fdc082a6613185c832173cc93
Subodh0 <70047623+Subodh0@users.noreply.github.com>
2023-10-24 21:55:24 +0530

    added contact section

--- src/components/contacts/contact.jsx
+++ src/components/contacts/contact.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
-import './contact.css'
-
+import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import {BsLinkedin} from 'react-icons/bs';
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>dummyemail@gmail.com</h5>
+            <a href="mailto:dummyemail@gmail.com" target='_blank'>Send an Email</a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <h5>Anita Sah</h5>
+            <a href="https://linkedin.com" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }

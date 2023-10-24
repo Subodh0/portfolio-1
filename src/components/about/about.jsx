ac123c3a80f1a3bbd73158f5b4f4414b5d06d88c
Subodh0 <70047623+Subodh0@users.noreply.github.com>
2023-10-24 16:29:59 +0530

    updated header section for mobile view and added about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import './about.css';
+import Me from '../../assets/me-about.jpg';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>0+ Years of Experience</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>500+ world wide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ completed projects</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente quis dolore quisquam eligendi fugiat sed aperiam dolores quam necessitatibus quibusdam possimus porro, deleniti, tempora minus repellendus explicabo impedit? Perspiciatis, voluptas.</p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
